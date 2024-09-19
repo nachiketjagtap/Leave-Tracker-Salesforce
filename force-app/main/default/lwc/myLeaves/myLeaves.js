140c3e7531ea2e81d868ab3a9621866d5d4a5d45
nachiketjagtap <nachiketjagtap9881@gmail.com>
2024-09-19 17:24:06 +0530

    Added object file manually and created apex classes to handle data

--- force-app/main/default/lwc/myLeaves/myLeaves.js
+++ force-app/main/default/lwc/myLeaves/myLeaves.js
@@ -1,3 +1,19 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from 'lwc';
+import getMyLeaves from '@salesforce/apex/LeaveRequstController.getMyLeaves';
 
-export default class MyLeaves extends LightningElement {}
+export default class MyLeaves extends LightningElement {
+    myLeaves = [];
+    myLeavesWiredResult;
+    @wire(getMyLeaves)
+    wiredMyLeaves(result) {
+        this.myLeavesWiredResult = result;
+        if(result.data){
+            this.myLeaves = result.data;
+        }
+        if (result.error) {
+
+            console.log('ERROR OCCURED FETCHING MY LEAVES - ', result.error);
+            
+        }
+    }
+}
