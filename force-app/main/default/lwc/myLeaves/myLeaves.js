e6898e125d3588be4328610c5c2a103b4d025888
nachiketjagtap <nachiketjagtap9881@gmail.com>
2024-09-20 11:30:25 +0530

    Added dummy data

--- force-app/main/default/lwc/myLeaves/myLeaves.js
+++ force-app/main/default/lwc/myLeaves/myLeaves.js
@@ -5,27 +5,32 @@ const COLUMNS = [
     {
         label: 'Request Id',
         fieldName: 'Name',
+        cellAttributes: { class: {fieldName:'cellClass'} },
         
     },
     {
         label: 'From Date',
         fieldName: 'From_Date__c',
+        cellAttributes: { class: {fieldName:'cellClass'} },
+    
         
     },
     {
         label: 'To Date',
         fieldName: 'To_Date__c',
+        cellAttributes: { class: {fieldName:'cellClass'} },
        
     },
     {
         label: 'Reason',
         fieldName: 'Reason__c',
+        cellAttributes: { class: {fieldName:'cellClass'} },
         
     },
     {
         label: 'Approval Status',
         fieldName: 'Status__c',
-        
+        cellAttributes: { class: {fieldName:'cellClass'} },
     },
     {
         label: 'Manager Comment',
@@ -39,8 +44,9 @@ const COLUMNS = [
             name: 'Edit',
             title: 'Edit',
             value: 'edit',
-           
-        }
+            disabled: {fieldName:'isEditDisabled'}
+        },
+        cellAttributes: { class: {fieldName:'cellClass'} }
     }
     
 ];
@@ -53,12 +59,21 @@ export default class MyLeaves extends LightningElement {
     wiredMyLeaves(result) {
         this.myLeavesWiredResult = result;
         if(result.data){
-            this.myLeaves = result.data;
+            this.myLeaves = result.data.map(a=>({
+                ...a,
+                cellClass: a.Status__c == 'Approved' ? 'slds-theme_success':a.Status__c == 'Rejected' ? 'slds-theme_warning':'',
+                isEditDisabled: a.Status__c!=='Pending'
+
+            }));
         }
         if (result.error) {
 
             console.log('ERROR OCCURED FETCHING MY LEAVES - ', result.error);
             
         }
+    }
+    get noRecordsFound(){
+        return this.myLeaves.length == 0;
+
     }
 }
